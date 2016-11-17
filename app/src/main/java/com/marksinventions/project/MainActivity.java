722f5b63bd398509d87bee0b614bbeafc5799cd0
Mark Banks <mark@marklylebanks.com>
2016-11-16 22:57:44 -0800

    Clean up code, remove longclick listener from gridlist, click calls dialog box with three choices, delete choice calls confirm delete dialog box.

--- app/src/main/java/com/marksinventions/project/MainActivity.java
+++ app/src/main/java/com/marksinventions/project/MainActivity.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     List<Set> setsList;
-    GridView  setsGrid ;
+    GridView setsGrid;
     SetsAdapter adapter;
     int setIndex = 0;
 
@@ -25,81 +25,46 @@ public class MainActivity extends AppCompatActivity {
         adapter = new SetsAdapter(this, setsList);
         setsGrid = (GridView) findViewById(R.id.setsGrid);
         setsGrid.setAdapter(adapter);
-       setsGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-           @Override
-           public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-               System.out.println("Click listener works. Set: " + i);
-               setIndex = i;
-               shortClick();
-              /* AlertDialog.Builder builder = new AlertDialog.Builder(getBaseContext());
-                builder.setMessage("Delete this set?")
-                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                setsList.remove(setIndex);
-                                adapter.notifyDataSetChanged();
-                            }
-                        })
-                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
-
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                dialogInterface.cancel();
-
-                            }
-                        });
-                AlertDialog dialog = builder.create();
-               dialog.show();*/
+        setsGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                System.out.println("Click listener works. Set: " + i);
+                setIndex = i;
+                shortClick();
             }
         });
-               setsGrid.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-
-                   @Override
-                   public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-                       System.out.println("Long Click listener works. Set: " + i);
-                       setsList.remove(i);
-                       adapter.notifyDataSetChanged();
-               /* AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setMessage("Delete this set?")
-                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                               setIndex = i;
-                                deleteSet();
-                            }
-                        })
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
-
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.cancel();
-
-                    }
-                });
-                AlertDialog dialog = builder.create();
-                dialog.show();
-                */
-
-                       return false;
-
-
-                   }
-               });
-
-           }
-
-
-
-    public  void deleteSet(int i){
-       setsList.remove(i);
-       // adapter.notifyDataSetChanged();
     }
+
+public void deleteSet(){
+    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    builder.setMessage("Confirm Delete")
+            .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                   setsList.remove(setIndex);
+                   adapter.notifyDataSetChanged();
+
+                }
+            })
+
+            .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+
+                }
+            });
+    AlertDialog dialog = builder.create();
+    dialog.show();
+
+}
     public void newProject(View view) {
     }
 
     public void newSet(View view) {
         setsList.add(new Set());
-        System.out.println(setsList.size()  + " elements");
+        System.out.println(setsList.size() + " elements");
         adapter.notifyDataSetChanged();
     }
 
@@ -115,28 +80,36 @@ public class MainActivity extends AppCompatActivity {
     public void stop(View view) {
     }
 
-    public static int  temp(int x, int y){
+    public static int temp(int x, int y) {
         return x * y;
     }
-
 
 
     public void shortClick() {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage("Delete this set?")
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                .setPositiveButton("Cancel", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        setsList.remove(setIndex);
-                        adapter.notifyDataSetChanged();
+                       // setsList.remove(setIndex);
+                        //adapter.notifyDataSetChanged();
+                       // deleteSet();
                     }
                 })
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
+                .setNeutralButton("Edit Set", new DialogInterface.OnClickListener() {
 
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                       // dialogInterface.cancel();
+
+
+                    }
+                })
+                .setNegativeButton("Delete Set", new DialogInterface.OnClickListener() {
+
+                   @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        deleteSet();
 
                     }
                 });
@@ -144,8 +117,7 @@ public class MainActivity extends AppCompatActivity {
         dialog.show();
 
 
-        // Toast.makeText(this, "short click", Toast.LENGTH_LONG).show();
-        }
-
     }
+
+}
 
